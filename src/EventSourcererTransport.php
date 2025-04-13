f8fe3bc48ce528a403e93db95474a10d1016429f
michaelbudd <michaelbudd6@gmail.com>
2025-04-13 10:55:13 +0200

    latest

--- src/EventSourcererTransport.php
+++ src/EventSourcererTransport.php
@@ -29,7 +29,7 @@ final readonly class EventSourcererTransport implements TransportInterface
             $eventSourcererApplicationId,
             $httpClient,
             $serializer
-        )
+        );
     }
 
     public function get(): iterable
