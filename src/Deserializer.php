2f0d0a08e4056bd361042f713808f1f52b75fee4
michaelbudd <michaelbudd6@gmail.com>
2025-04-18 08:53:35 +0100

    adds deserializer

---
+++ src/Deserializer.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PearTreeWebLtd\EventSourcererSymfonyMessengerTransport;
+
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\Transport\Serialization\SerializerInterface;
+
+final class Deserializer implements SerializerInterface
+{
+    public function decode(array $encodedEnvelope): Envelope
+    {
+        // TODO: Implement decode() method.
+    }
+
+    public function encode(Envelope $envelope): array
+    {
+        // TODO: Implement encode() method.
+    }
+}
