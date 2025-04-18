699d72f00cadb91d51a0bf2352c1ce399ab2d26c
michaelbudd <michaelbudd6@gmail.com>
2025-04-18 09:02:18 +0100

    wip

---
+++ src/ProcessEvent.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PearTreeWebLtd\EventSourcererSymfonyMessengerTransport;
+
+final readonly class ProcessEvent
+{
+    public function __construct($event) {}
+}
