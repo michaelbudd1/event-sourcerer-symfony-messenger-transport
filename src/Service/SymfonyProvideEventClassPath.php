27f1ae85c8463c6b708f468dc703b5ac6806cb64
mike <michaelbudd6@gmail.com>
2026-07-05 09:03:53 +0100

    wip

--- src/Service/SymfonyProvideEventClassPath.php
+++ src/Service/SymfonyProvideEventClassPath.php
@@ -7,10 +7,16 @@ namespace PearTreeWeb\EventSourcerer\SymfonyClient\Service;
 use PearTreeWeb\EventSourcerer\Common\Model\EventName;
 use PearTreeWeb\EventSourcerer\Common\Service\ProvideEventClassPath;
 
-final class SymfonyProvideEventClassPath implements ProvideEventClassPath
+final readonly class SymfonyProvideEventClassPath implements ProvideEventClassPath
 {
+    public function __construct(private iterable $eventTemplates)
+    {
+
+    }
+
     public function for(EventName $eventName): string
     {
+        dd($eventTemplates);
         // TODO: Implement for() method.
     }
 }
